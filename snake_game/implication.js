edfe2a07bae2c6608109630aa626ed2108edab01
mrnishant1 <nishantkumaragra@gmail.com>
2026-02-05 00:33:18 +0530

    its done

--- snake_game/implication.js
+++ snake_game/implication.js
@@ -4,11 +4,7 @@ const game = new Scene(canvas, context);
 //snake is Linked List consHeads nodes named as "Game_Objects"
 const snake_body = new snake();
 //appendLL is function to append node in LinkedList
-snake_body.appendLL(game,200,200);
 snake_body.appendLL(game,0,0);
-// snake_body.appendLL(game);
-// snake_body.appendLL(game);
-console.log(snake_body.Head,"hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
 
 //setGameObject -- helps to push snake part to snake_body[] in game world 
 //help in render game_Objects in world 
@@ -16,13 +12,18 @@ game.setGameObject(snake_body);
 
 //Keypress for direction
 window.addEventListener("keydown", (e) => {
-  snake_body.Head.setDirection(e.code);
+  if(e.code!="ArrowLeft"&&e.code!="ArrowRight"&&e.code!="ArrowUp"&&e.code!="ArrowDown") {console.log(e.code); return}
+  game.direction=e.code;
 });
 
 //to animate continuous
 function animate() {
   game.renderAnimation(snake_body.Head);
-  requestAnimationFrame(animate);
+  // requestAnimationFrame(animate);
 }
 
-requestAnimationFrame(animate);
+setInterval(() => {
+  animate()
+}, 300);
+
+// requestAnimationFrame(animate);
--- snake_game/index.js
+++ snake_game/index.js
@@ -10,8 +10,9 @@ class Scene {
     this.entities = [];
     this.stopAnimate = false;
     this.food = new class_food(this);
+    this.direction = "";
 
-    this.snake_speed = 2;
+    this.snake_speed = 50;
 
     this.snake_body = null;
   }
@@ -37,20 +38,22 @@ class Scene {
     const y2 = this.food.posY + this.food.height / 2;
     const distance = Math.hypot(x2 - x1, y2 - y1); // use + inside sqrt
     // console.log(x2,x1,y2,y1);
+    console.log(distance);
 
     if (distance <= 70) {
       this.food.spawn();
       this.snake_speed += 0.005;
-      this.entities.push(
-        new Game_object(this, snake.posX - snake.width, snake.posY),
+      this.snake_body.appendLL(
+        this,
+        this.snake_body.Head.posX,
+        this.snake_body.Head.posY,
       );
-      console.log(this.entities);
     }
   }
 
   renderAnimation() {
     if (this.stopAnimate) return;
-    // this.checkCollision();
+    this.checkCollision();
     this.clearCanvas();
     this.food.draw();
     this.snake_body.moveHead_rest_follow(this.snake_speed);
@@ -69,33 +72,49 @@ class snake {
 
     if (!this.Head) {
       this.Head = newNode;
+      newNode.color="red"
     } else {
       newNode.next = this.Head;
       this.Head = newNode;
     }
   }
-  moveAll(speed) {
+
+  moveHead_rest_follow(speed
+  ) {
+    let tempX = this.Head.posX;
+    let tempY = this.Head.posY;
     let current = this.Head;
+    this.Head.move(speed);
+    // this.Head.color="red"
+
     while (current) {
       current.draw();
-      current.move(speed);
-      current = current.next;
-    }
-  }
-  moveHead_rest_follow(speed) {
-    let current = this.Head;
-    current.draw();
-    while (current) {
+      current.color="black"
       if (current.next) {
-        current.posX = current.next.posX+200;
-        current.posY = current.next.posY+200;
-      } else {
-        current.move(speed);
+        //just checking 
+        if (
+          current.next.posX == this.Head.posX &&
+          current.next.posY == this.Head.posY
+        ) {
+          // scene.gameOver();
+          current.next=null
+          console.log("game over");
+          return;
+        }
+        let anotherTempX = current.next.posX;
+        let anotherTempY = current.next.posY;
+        current.next.posX = tempX;
+        current.next.posY = tempY;
+        tempX = anotherTempX;
+        tempY = anotherTempY;
       }
       current = current.next;
     }
-    console.log(this.Head);
   }
+
+  // Check_hit_itself(){
+
+  // }
 }
 
 class Game_object {
@@ -103,44 +122,45 @@ class Game_object {
     //this next points to next node ("game_object")
     this.next = null;
 
-    this.height = 100;
-    this.width = 100;
-    this.color = "red";
+    this.height = 50;
+    this.width = 50;
+    this.color = "black";
     this.scene = scene;
     this.posX = posX || 0;
     this.posY = posY || 0;
-    this.direction = "";
   }
   draw() {
     this.scene.context.fillRect(this.posX, this.posY, this.width, this.height);
-  }
-  setDirection(direction) {
-    this.direction = direction;
+    this.scene.context.fillStyle = this.color;
   }
 
-  // checkCollision() {
-  //   this.posX > this.scene.canvas.width - this.width ||
-  //   this.posX < 0 ||
-  //   this.posY > this.scene.canvas.height - this.height ||
-  //   this.posY < 0
-  //     ? this.scene.gameOver()
-  //     : "";
-  // }
+  checkCollision() {
+    this.posX > this.scene.canvas.width - this.width ||
+    this.posX < 0 ||
+    this.posY > this.scene.canvas.height - this.height ||
+    this.posY < 0
+      ? this.scene.gameOver()
+      : "";
+  }
 
   move(speed) {
-    // this.checkCollision();
+    this.checkCollision();
 
-    switch (this.direction) {
+    switch (game.direction) {
       case "ArrowLeft":
+        if (game.direction == "ArrowRight") break;
         this.posX -= speed;
         break;
       case "ArrowRight":
+        if (game.direction == "ArrowLeft") break;
         this.posX += speed;
         break;
       case "ArrowUp":
+        if (game.direction == "ArrowDown") break;
         this.posY -= speed;
         break;
       case "ArrowDown":
+        if (game.direction == "ArrowUp") break;
         this.posY += speed;
         break;
     }
@@ -154,10 +174,12 @@ class class_food {
     this.height = 50;
     this.posX = Math.random() * this.scene.canvas.width - this.width;
     this.posY = Math.random() * this.scene.canvas.height - this.height;
+    // this  
   }
 
   draw() {
     this.scene.context.fillRect(this.posX, this.posY, this.width, this.height);
+    this.scene.context.fillStyle="black"
   }
   move() {}
   spawn() {
